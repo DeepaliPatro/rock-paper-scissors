9a553409be86015b3d55e50c7c97bc82f2e19b2f
Deepali Patro <deepaliparo86@gmail.com>
2024-05-20 09:15:39 -0400

    styles formatted to make the UI responsive

--- src/components/GameDisplay.vue
+++ src/components/GameDisplay.vue
@@ -9,9 +9,9 @@
     };
 </script>
 <template>
-    <div class="flex items-center justify-center gap-2 sm:gap-3 md:gap-5 mx-2 sm:mx-3 md:mx-5">
-        <button @click="play('rock')" class="text-4xl sm:text-6xl lg:text-8xl rounded-full max-w-fit p-3 sm:p-4 lg:p-5 bg-white text-white shadow-lg shadow-slate-600 hover:bg-slate-200 hover:shadow-lg hover:shadow-slate-600 active:shadow-none transition-all duration-300">👊</button>
-        <button @click="play('paper')" class="text-4xl sm:text-6xl lg:text-8xl rounded-full max-w-fit p-3 sm:p-4 lg:p-5 bg-white text-white shadow-lg shadow-slate-600 hover:bg-slate-200 hover:shadow-lg hover:shadow-slate-600 active:shadow-none transition-all duration-300">🫳</button>
-        <button @click="play('scissors')" class="text-4xl sm:text-6xl lg:text-8xl rounded-full max-w-fit p-3 sm:p-4 lg:p-5 bg-white text-white shadow-lg shadow-slate-600 hover:bg-slate-200 hover:shadow-lg hover:shadow-slate-600 active:shadow-none transition-all duration-300">✌️</button>
+    <div class="flex items-center justify-center gap-3 sm:gap-3 md:gap-5 mx-2 sm:mx-3 md:mx-5">
+        <button @click="play('rock')" class="text-6xl sm:text-6xl lg:text-8xl rounded-full max-w-fit p-3 sm:p-4 lg:p-5 bg-white text-white shadow-lg shadow-slate-600 hover:bg-slate-200 hover:shadow-lg hover:shadow-slate-600 active:shadow-none transition-all duration-300">👊</button>
+        <button @click="play('paper')" class="text-6xl sm:text-6xl lg:text-8xl rounded-full max-w-fit p-3 sm:p-4 lg:p-5 bg-white text-white shadow-lg shadow-slate-600 hover:bg-slate-200 hover:shadow-lg hover:shadow-slate-600 active:shadow-none transition-all duration-300">🫳</button>
+        <button @click="play('scissors')" class="text-6xl sm:text-6xl lg:text-8xl rounded-full max-w-fit p-3 sm:p-4 lg:p-5 bg-white text-white shadow-lg shadow-slate-600 hover:bg-slate-200 hover:shadow-lg hover:shadow-slate-600 active:shadow-none transition-all duration-300">✌️</button>
     </div>
 </template>
